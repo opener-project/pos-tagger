bdbbdb20380b23c60f14acf77410a36f4ed7ce74
Giannis Melidis <giannismelidis@olery.com>
2013-06-12 15:27:50 +0200

    Changes to the tokenizer component.

--- lib/opener/pos_tagger.rb
+++ lib/opener/pos_tagger.rb
@@ -1,7 +1,44 @@
-require "pos_tagger/version"
+require_relative "pos_tagger/version"
+require "opener/pos_taggers/base"
+require "opener/pos_taggers/en"
+require "opener/pos_taggers/fr"
+require "opener/pos_taggers/es"
+require "opener/pos_taggers/it"
+require "nokogiri"
+require "open3"
 
 module Opener
   class POSTagger
-      # Your code goes here...
+    attr_reader :args
+    attr_accessor :options
+        
+    def tag(text)
+      language = get_kaf_language(text)      
+      pos_tagger = pos_tagger_for_language(language)
+      output, error, process = Open3.capture3(pos_tagger.command, :stdin_data=>text)
+    end
+    
+    alias :run :tag
+    
+    protected
+
+    def pos_tagger_for_language(language)
+      Opener::POSTaggers.const_get(language.upcase).new
+    end
+    
+    def get_kaf_language(text)
+      reader = Nokogiri::XML::Reader(text)
+      language = reader.read.lang
+      check_language_support(language)
+      return language
+    end
+    
+    def check_language_support(language)
+      abort "'#{language}' language is not supported." if !language_list.include?(language.downcase)
+    end
+      
+    def language_list
+      ['en', 'fr', 'es', 'it', 'de', 'nl']
+    end
   end
 end
