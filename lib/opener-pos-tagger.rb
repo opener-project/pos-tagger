1b3272ce4f30471f6d253aba3201d44f1269c405
Giannis Melidis <giannismelidis@olery.com>
2013-06-12 11:37:52 +0200

    Initial commit

---
+++ lib/opener-pos-tagger.rb
@@ -0,0 +1,9 @@
+require "opener-pos-tagger/version"
+
+module Opener
+  module Pos
+    module Tagger
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/opener-pos-tagger/version.rb
@@ -0,0 +1,7 @@
+module Opener
+  module Pos
+    module Tagger
+      VERSION = "0.0.1"
+    end
+  end
+end
